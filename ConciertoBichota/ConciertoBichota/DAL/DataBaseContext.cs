34068551e5dd7c39d5ac236a3c87003226409420
Santiagonohaba@gmail.com <Santiagonohaba@gmail.com>
2023-04-16 08:56:00 -0500

    Creating and configuration Database for project ConciertoBichota

---
+++ ConciertoBichota/ConciertoBichota/DAL/DataBaseContext.cs
@@ -0,0 +1,20 @@
+﻿using ConciertoBichota.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+
+namespace ConciertoBichota.DAL
+{
+    public class DataBaseContext : DbContext 
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+        }
+        public DbSet<ConcertDB> Tickets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ConcertDB>().HasIndex(c => c.Id).IsUnique();
+        }
+    }
+}
---
+++ ConciertoBichota/ConciertoBichota/DAL/Entities/ConcertDB.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConciertoBichota.DAL.Entities
+{
+    public class ConcertDB
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime? UseDate { get; set; }
+        public bool IsUsed { get; set; }
+        public string EntranceGate { get; set; }
+    }
+}
