10fc44bb479bfff82cd71908e23d54744b411f3f
Santiagonohaba@gmail.com <Santiagonohaba@gmail.com>
2023-10-08 00:35:10 -0500

    Data Base Created And Configured With Entity Framework Core

---
+++ Parcial2_NohabaZajarSantiago/DAL/DataBaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial2_NohabaZajarSantiago.DAL.Entities;
+
+namespace Parcial2_NohabaZajarSantiago.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base (options)
+        {
+
+        }
+        //Control Duplicidad
+        protected override void OnModelCreating (ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<NaturalPerson>().HasIndex(c => c.Identification).IsUnique();
+        }
+        //Mapeo mi identidad para convertirla en una tabla
+        public DbSet<NaturalPerson> NaturalPersons { get; set;}
+
+    }
+}
---
+++ Parcial2_NohabaZajarSantiago/Migrations/20231008052853_InitialDataBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Parcial2_NohabaZajarSantiago.Migrations
+{
+    public partial class InitialDataBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NaturalPersons",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Identification = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    BirthYear = table.Column<int>(type: "int", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NaturalPersons", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NaturalPersons_Identification",
+                table: "NaturalPersons",
+                column: "Identification",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NaturalPersons");
+        }
+    }
+}
