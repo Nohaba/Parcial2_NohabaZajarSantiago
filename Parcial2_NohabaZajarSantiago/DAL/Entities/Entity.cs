fd70124170a78e9089e3c6fd21089e84045b91bb
Santiagonohaba@gmail.com <Santiagonohaba@gmail.com>
2023-10-07 23:45:35 -0500

    Class Entity And Class NaturalPerson Created With Properties

---
+++ Parcial2_NohabaZajarSantiago/DAL/Entities/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial2_NohabaZajarSantiago.DAL.Entities
+{
+    public class Entity
+    {
+        [Key]
+        public virtual Guid Id { get; set; }
+
+        [Display(Name = "Fecha de creacion")]
+        public virtual DateTime? CreatedDate { get; set; }
+
+        [Display(Name = "Fecha de modificacion")]
+        public virtual DateTime? ModifiedDate { get; set; }
+    }
+}
---
+++ Parcial2_NohabaZajarSantiago/DAL/Entities/NaturalPerson.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial2_NohabaZajarSantiago.DAL.Entities
+{
+    public class NaturalPerson : Entity
+    {
+        #region Properties
+        [Display(Name = "Identificacion")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe de tener maximo {1} caracteres ")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public String Identification { get; set; }
+        
+        [Display(Name = "Nombre Completo")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe de tener maximo {1} caracteres ")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Correo Electronico")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe de tener maximo {1} caracteres ")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "Año De Nacimiento")]
+        [Required(ErrorMessage = "El campo {0} es Requerido")]
+        public int BirthYear { get; set; }
+
+        [Display(Name = "Edad")]
+        public int Age { get; set; }
+
+        #endregion
+
+    }
+}
